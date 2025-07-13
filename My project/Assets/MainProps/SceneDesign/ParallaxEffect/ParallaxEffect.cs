3d21fcd7a9ffaaf0ce68e21077eb1563560b6b8d
pwoopy <paddymanics@gmail.com>
2025-07-13 13:40:27 +0800

    Added a scene for testing design
    Testing still

---
+++ My project/Assets/MainProps/SceneDesign/ParallaxEffect/ParallaxEffect.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ParallaxEffect : MonoBehaviour
+{
+    Material mat;
+    float distance;
+
+    [Range(0f, 0.5f)]
+    public float speed = 0.2f;
+
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+
+    }
+
+    void Update()
+    {
+        distance += Time.deltaTime * speed;
+        mat.SetTextureOffset("_MainTex", Vector2.right * distance);
+    }
+}
